e846a6e73e2494bdeaeebe21062c1faf206b0d3f
PureRandomGit <67933247+PureRandomGit@users.noreply.github.com>
2024-10-03 17:53:42 -0400

    Update ledDigital.ino

--- ledDigital.ino
+++ ledDigital.ino
@@ -2,9 +2,12 @@
 
 #define NUM_LEDS 66
 #define LED_PIN 4 //Pin D2
-#define INPUT_PIN 5 //Pin D5
+#define INPUT_PIN 2 //Pin D5
 
 CRGB leds[NUM_LEDS];
+
+// Define custom RGB value for orange
+CRGB customOrange = CRGB(255, 30, 0); // Example: RGB value for orange
 
 void setup() {
     // Initialize the FastLED library with the specified pin
@@ -23,11 +26,11 @@ void loop() {
     if (inputState == HIGH) {
         color = CRGB::Green;  // Set to green if input is HIGH
     } else {
-        color = CRGB::Orange; // Default to orange
+        color = customOrange;  // Use custom RGB value for orange
     }
 
     // Apply the color to all LEDs
-    for(int i = 0; i < NUM_LEDS; i++) {
+    for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = color;
     }
 
